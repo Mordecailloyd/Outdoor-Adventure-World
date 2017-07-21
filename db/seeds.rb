c3c932c38ce0ac10ee1e8aa31800adadb03ee399
Nicholas Bacon <mordecailloyd@gmail.com>
2017-07-20 18:03:22 -0700

    controllers and views done for back end, begining seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(:username => "Nicholas", :password => "password")
+
+nick = User.create!(:username => "Nicholas", :password => "password")
+fleece = Product.create!(:title => "Momentum Full-Zip Fleece Jacket", :body =>
+  "Layer up for your early-morning approaches with The North Face Momentum
+  full-zip fleece jacket. It offers just the right amount of warmth and protection
+  for active endeavors in the cold.", :price => 67.99, :brand => "The North Face",
+  :image_url = "https://www.rei.com/media/f6d9248f-5e7f-4343-8fcb-b91aa032d943" )
